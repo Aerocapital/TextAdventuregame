ae4338a7c04f8f5e7bf6b97744b0504f2ec6ce2d
andreas <andreas.ehret82@gmail.com>
2025-05-12 16:14:59 +0200

    Projekttag 2: Charakterauswahl mit Attributen, Spieler Klasse und Grundgerüstaufbau

--- Projekt/Program.cs
+++ Projekt/Program.cs
@@ -1,2 +1,82 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ProjektInventory;
+
+class Programm
+{
+    static Spieler spielerCharakter;
+    static void Main(string[] args)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.WriteLine("======================================");
+        Console.WriteLine("Willkommen In der Welt von  Ozelot!");
+        Console.WriteLine("======================================");
+
+        Console.ResetColor();
+
+        Console.WriteLine("Eine dunkle Bedrohung nähert sich Ozelot");
+        Console.WriteLine("Ein Held wird gesucht um die Bürger und Ländereien zu verteidigen");
+
+        Console.WriteLine("Bevor das Abenteuer beginnt, wähle deine Helden: ");
+
+        string ausgewaehlteKlasse = "Bauer";        //Standartklasse
+        int anfangsStaerke = 1;                //Standartstaerke
+        int anfangsIntelligenz = 1;            //Standartintelligenz
+
+        bool charakterwahl = false;    // Variable für die Charakterwahl
+        while (!charakterwahl)
+        {
+            Console.WriteLine("Wähle deine Klasse: ");
+            Console.WriteLine("1. Krieger");
+            Console.WriteLine("2. Magier");
+            Console.WriteLine("3. Bauer");
+            Console.WriteLine("Bitte gib die Nummer deiner Wahl ein: ");                //Nachträglich eingesetzt da schöner in der Konsole
+            string eingabe = Console.ReadLine();
+            switch (eingabe)
+            {
+                case "1":
+                    ausgewaehlteKlasse = "Krieger";
+                    anfangsStaerke = 10;
+                    anfangsIntelligenz = 5;
+                    charakterwahl = true;
+                    break;
+                case "2":
+                    ausgewaehlteKlasse = "Magier";
+                    anfangsStaerke = 5;
+                    anfangsIntelligenz = 10;
+                    charakterwahl = true;
+                    break;
+                case "3":
+                    ausgewaehlteKlasse = "Bauer";
+                    anfangsStaerke = 1;
+                    anfangsIntelligenz = 1;
+                    charakterwahl = true;
+                    break;
+                default:
+                    Console.WriteLine("Ungültige Eingabe, bitte wähle eine gültige Klasse.");
+                    break;
+            }
+            
+        }
+        Console.WriteLine($"\nDu hast dich fpr {ausgewaehlteKlasse} entschieden");
+
+        Console.WriteLine("Bitte nenne mir deinen Namen, Held von Ozelot: ");
+
+        string spielerName = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(spielerName))              //IsnullorEMpty vorgabe von visual studio
+        {
+            Console.WriteLine($"Du bist fortan bekannt als {spielerName} );");
+            spielerName = "Helz von Ozelot"; // Standardname
+        }
+
+        // --- Spieler-Objekt erstellen ---
+        // Jetzt erstellen wir das Spieler-Objekt mit den gesammelten Infos
+        spielerCharakter = new Spieler(spielerName, ausgewaehlteKlasse, anfangsStaerke, anfangsIntelligenz);
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("\nDein Abenteuer in Azeria beginnt, Held!");
+        Console.ResetColor();
+        Console.WriteLine("--------------------------------------------------");
+
+
+    }
+}
--- Projekt/obj/Debug/net9.0/Projekt.AssemblyInfo.cs
+++ Projekt/obj/Debug/net9.0/Projekt.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3ef803e1161891b4b617576836a384d4016248c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee39804eccb6b9e8a8cc37a8ce832c6841cb339b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
